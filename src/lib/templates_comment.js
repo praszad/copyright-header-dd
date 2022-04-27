55dac3280ec23c1c0d2f5759c2bf17f59862810a
Prasad Babu <silverprasad01@gmail.com>
2022-04-27 14:07:02 +0530

    Create templates_comment.js

---
+++ src/lib/templates_comment.js
@@ -0,0 +1,4 @@
+exports.info = `* IBM Confidential
+* 694985D
+* © Copyright IBM Corp. 2022
+* US Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.`
